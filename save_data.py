8e4b192ee72c65de9e5312a6e341431fd54f9ed9
David Smith <smithdc@gmail.com>
2020-10-14 20:34:20 +0100

    Skip loading of empty_file

--- save_data.py
+++ save_data.py
@@ -21,11 +21,12 @@ def save_data():
     json_folder = Path("json_output/")
     data = []
     for file in json_folder.iterdir():
-        with open(file) as json_file:
-            benchmark = flatten_json(json.load(json_file))
-            del benchmark["control_data"]
-            del benchmark["experiment_data"]
-            data.append(benchmark)
+        if file.suffix == ".json":
+            with open(file) as json_file:
+                benchmark = flatten_json(json.load(json_file))
+                del benchmark["control_data"]
+                del benchmark["experiment_data"]
+                data.append(benchmark)
 
     db = Database("django.db")
     db['bench'].insert_all(data)
