f7b5afad342fa8992c16f9e26d8a78e70f4e6164
David Smith <smithdc@gmail.com>
2020-10-14 20:23:47 +0100

    Auto run benchmarks and save data

--- run_benchmarks.py
+++ run_benchmarks.py
@@ -1,8 +1,25 @@
 from sqlite_utils import Database
-from djangobench.main import discover_benchmarks, DEFAULT_BENCHMARK_DIR
+from djangobench.main import discover_benchmarks, DEFAULT_BENCHMARK_DIR, run_benchmarks
+import os
 
-db = Database("django.db")
 
-commits = [commit['sha'] for commit in db["commits"].rows_where(select="sha")]
-benchmarks = discover_benchmarks(DEFAULT_BENCHMARK_DIR)
+def run_bench():
+    db = Database("django.db")
 
+    commits = [commit['sha'] for commit in db["commits"].rows_where(select="sha", order_by="committer_date desc")]
+    benchmarks = list(discover_benchmarks(DEFAULT_BENCHMARK_DIR))
+    tested_commits = {commit['experiment_commit'] for commit in db["bench"].rows_where(select="experiment_commit")}
+
+    for idx, commit in enumerate(commits):
+        if commit not in tested_commits:
+            experiment = commit
+            control = commits[idx + 1]
+            break
+
+    print(control, experiment)
+    os.chdir('django')
+    run_benchmarks(control, experiment, DEFAULT_BENCHMARK_DIR, benchmarks, 5, vcs='git', record_dir="../json_output")
+
+
+if __name__ == '__main__':
+    run_bench()
--- save_data.py
+++ save_data.py
@@ -17,11 +17,19 @@ def flatten_json(y):
     return out
 
 
-json_folder = Path("json_output/")
-data = []
-for file in json_folder.iterdir():
-    with open(file) as json_file:
-        data.append(flatten_json(json.load(json_file)))
+def save_data():
+    json_folder = Path("json_output/")
+    data = []
+    for file in json_folder.iterdir():
+        with open(file) as json_file:
+            benchmark = flatten_json(json.load(json_file))
+            del benchmark["control_data"]
+            del benchmark["experiment_data"]
+            data.append(benchmark)
 
-db = Database("django.db")
-db['bench'].insert_all(data)
+    db = Database("django.db")
+    db['bench'].insert_all(data)
+
+
+if __name__ == '__main__':
+    save_data()
